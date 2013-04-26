b569816ba44b6d0e3af91fa36de09579351fd86d
Pavel Osadchyi <posadchiy@gmail.com>
2013-04-26 10:14:33 +0300

    Initial commit

---
+++ src/main/java/com/example_osadchyi/HelloWorldREST.java
@@ -0,0 +1,17 @@
+// //////////////////////////////////////
+// BMC Software, Inc.
+// Confidential and Proprietary
+// Copyright (c) BMC Software, Inc. 2013
+// All Rights Reserved.
+// //////////////////////////////////////
+package com.example_osadchyi;
+
+/**
+ * User: paul
+ * Date: 4/25/13
+ * Time: 11:51 PM
+  */
+
+public class HelloWorldREST {
+
+}
